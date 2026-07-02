21943d8677c5a47386cb2b9f7d4c6df69abb8692
soonbok <ddu3423@gmail.com>
2026-07-02 23:05:28 +0900

    feat: add headroom computation and guidance

---
+++ lib/analysis/headroom.dart
@@ -0,0 +1,22 @@
+import '../models/person_box.dart';
+
+class HeadroomAdvice {
+  final double ratio;
+  final String hint;
+  const HeadroomAdvice({required this.ratio, required this.hint});
+}
+
+/// 인물 머리 위 여백 비율을 계산하고 카메라 상하 조정을 안내.
+HeadroomAdvice computeHeadroom(PersonBox person,
+    {double idealMin = 0.05, double idealMax = 0.15}) {
+  final ratio = person.top;
+  String hint;
+  if (ratio < idealMin) {
+    hint = '카메라를 살짝 올리세요';
+  } else if (ratio > idealMax) {
+    hint = '카메라를 살짝 내리세요';
+  } else {
+    hint = '';
+  }
+  return HeadroomAdvice(ratio: ratio, hint: hint);
+}
