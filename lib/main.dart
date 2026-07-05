e60e20bbccaec2c58f2d64803bf2d45452090018
soonbok <ddu3423@gmail.com>
2026-07-05 23:58:25 +0900

    chore: kakao_flutter_sdk_user 의존성 + KakaoSdk 초기화

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
+import 'community/kakao_config.dart';
 import 'firebase_options.dart';
 import 'screens/camera_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  KakaoSdk.init(nativeAppKey: kakaoNativeAppKey);
   await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   try {
     await Firebase.initializeApp(
